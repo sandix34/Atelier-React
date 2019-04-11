8d478c0bced12227aaf16e7b16118fc5ac116620
sandrine <sandrinezocca@gmail.com>
2019-04-11 17:11:20 +0200

    MovieList and MovieDetails layout

--- src/components/movie-list/MovieList.js
+++ src/components/movie-list/MovieList.js
@@ -3,6 +3,27 @@ import React, { Component } from 'react';
 export default class MovieList extends Component {
 
     render() {
-        return <div className="w-75">Movie List</div>
+        return (
+            <div className="w-75 d-flex flex-row flex-wrap align-content-start">
+                <div className="w-25 p-2">
+                    <div className="border">Film</div>
+                </div>
+                <div className="w-25 p-2">
+                    <div className="border">Film</div>
+                </div>
+                <div className="w-25 p-2">
+                    <div className="border">Film</div>
+                </div>
+                <div className="w-25 p-2">
+                    <div className="border">Film</div>
+                </div>
+                <div className="w-25 p-2">
+                    <div className="border">Film</div>
+                </div>
+                <div className="w-25 p-2">
+                    <div className="border">Film</div>
+                </div>
+            </div>
+        );
     }
 }

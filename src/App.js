bf1b6b9ce1213a1808018fec27c49ad1730a42d6
sandrine <sandrinezocca@gmail.com>
2019-04-11 15:41:12 +0200

    import components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import { Header, MovieList, MovieDetails } from './components';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+      <Header />
+      <MovieList />
+      <MovieDetails />
       </div>
     );
   }

0756d37a7c150971b0aa8bf8bc2c2c5074d1ac69
sandrine <sandrinezocca@gmail.com>
2019-04-12 15:21:01 +0200

    state + changing the style for MovieElement

--- src/components/movie-list/MovieElement.js
+++ src/components/movie-list/MovieElement.js
@@ -4,12 +4,13 @@ export default class MovieElement extends Component {
 
     render() {
         return (
-            <div className="w-25 p-2">
-                <div className="card">
-                    <img src="http://t1.gstatic.com/images?q=tbn:ANd9GcQRldT4RKBlDSiPcwHAZbLAUNqsGCatYti-CLyeCxCsG9iADdMq" alt="film" className="card-img-top"/>
-                    <div className="card-body">
-                        <h5 className="card-title">AVENGERS</h5>
-                        <p className="card-text">Nick Fury, le directeur du S.H.I.E.L.D., l'organisation qui préserve la paix dans le monde, veut former une équipe d'irréductibles pour empêcher la destruction du monde. Iron Man, Hulk, Thor, Captain America, Hawkeye et Black Widow répondent présents.</p>
+            <div className="w-50 p-2">
+                <div className="border d-flex">
+                    <img width="150" height="200" src="http://t1.gstatic.com/images?q=tbn:ANd9GcQRldT4RKBlDSiPcwHAZbLAUNqsGCatYti-CLyeCxCsG9iADdMq" alt="film"/>
+                    <div className="flex-fill d-flex flex-column p-3">
+                        <h5>Titre</h5>
+                        <hr className="w-100"/>
+                        <p>Nick Fury, le directeur du S.H.I.E.L.D., l'organisation qui préserve la paix dans le monde, veut former une équipe d'irréductibles pour empêcher la destruction du monde. Iron Man, Hulk, Thor, Captain America, Hawkeye et Black Widow répondent présents.</p>
                     </div>
                 </div>
             </div>
